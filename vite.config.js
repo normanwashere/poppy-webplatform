00e9539f5ab7d0170058c415a5a8468be576a6b3
normanwashere <normanarisdeocareza@gmail.com>
2025-06-21 17:45:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-export default defineConfig({ plugins: [sveltekit()] });
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+};
